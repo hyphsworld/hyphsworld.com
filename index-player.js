ada439f8f4ff39f33ecf3795c46b2c47b56e69af
hyphsworld <hyphsworld3d@gmail.com>
2026-04-27 22:04:17 -0700

    Delete index-player.js

--- index-player.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-const el=document.getElementById('player');
-if(el){
-el.innerHTML='<button onclick="alert(\'Playing HAM\')">PLAY HAM</button>';
-}
-console.log('Homepage player ready');
-});
