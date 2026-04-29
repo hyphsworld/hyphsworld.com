810aac0d181c8e8ea666ecbcd1af65b301056cb4
hyphsworld <hyphsworld3d@gmail.com>
2026-04-29 10:20:34 -0700

    Update vault-access.js

--- vault-access.js
+++ vault-access.js
@@ -1,17 +1,6 @@
-// vault-access.js
-
-const codes = {
-    AMSWEST: 'c05cf473094ca91f8e88337f4b1d504467b133ff8c2c38a92af912bff6ffd50f', // SHA-256 hash
-    WORLD5:  'f81dd8a8f53195f2c55fb0917d03602f461b4e2fb18e02f0138ff39d36a5b680', // SHA-256 hash
+const vaultAccess = {
+  AMSWEST: 'c87b9dbf9ff6ea5298c8e1740b96492b3743b70f3672faa2a412052f28b709bb',
+  WORLD5: '817a09d3d2d6e6bc6d869f6abb993786a8ed89e076d42692be31e0c7a0c4b68c'
 };
 
-async function verifyCode(inputCode) {
-    const encoder = new TextEncoder();
-    const inputHashBuffer = await crypto.subtle.digest('SHA-256', encoder.encode(inputCode));
-    const inputHashArray = Array.from(new Uint8Array(inputHashBuffer));
-    const inputHash = inputHashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-
-    return Object.values(codes).includes(inputHash);
-}
-
-module.exports = { verifyCode };
+module.exports = vaultAccess;
