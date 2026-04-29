2a8636dea43bb880253487b2fcf35566cf79138c
hyphsworld <hyphsworld3d@gmail.com>
2026-04-29 10:57:36 -0700

    Delete vault-access.js

--- vault-access.js
+++
@@ -1,6 +0,0 @@
-const vaultAccess = {
-  AMSWEST: 'c87b9dbf9ff6ea5298c8e1740b96492b3743b70f3672faa2a412052f28b709bb',
-  WORLD5: '817a09d3d2d6e6bc6d869f6abb993786a8ed89e076d42692be31e0c7a0c4b68c'
-};
-
-module.exports = vaultAccess;
